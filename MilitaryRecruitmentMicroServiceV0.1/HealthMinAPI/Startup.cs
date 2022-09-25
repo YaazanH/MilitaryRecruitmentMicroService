d637fc044921d8fe858cf9ef32e341040b10725d
YaazanH <yazon.hamwi@gmail.com>
2022-09-25 21:22:06 +0300

    q

--- MilitaryRecruitmentMicroServiceV0.1/HealthMinAPI/Startup.cs
+++ MilitaryRecruitmentMicroServiceV0.1/HealthMinAPI/Startup.cs
@@ -1,16 +1,11 @@
+using Consul;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HealthMinAPI
 {
@@ -32,6 +27,15 @@ namespace HealthMinAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HealthMinAPI", Version = "v1" });
             });
+
+            services.AddSingleton<IHostedService, ConsulRegisterService>();
+            services.Configure<ServiceConfiguration>(Configuration.GetSection("Test"));
+            services.Configure<ConsulConfiguration>(Configuration.GetSection("Consul"));
+
+            var consulAddress = Configuration.GetSection("Consul")["Url"];
+
+            services.AddSingleton<IConsulClient, ConsulClient>(provider =>
+                new ConsulClient(config => config.Address = new Uri(consulAddress)));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
