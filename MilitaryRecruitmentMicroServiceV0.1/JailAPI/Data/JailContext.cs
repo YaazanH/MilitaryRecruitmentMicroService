560e23c7a50c98bd7d793b0100acabd19b9f4fcf
BeanDady <mohammadkatramiz607@gmail.com>
2022-10-08 21:06:56 +0300

    Add Jail

---
+++ MilitaryRecruitmentMicroServiceV0.1/JailAPI/Data/JailContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JailAPI.Data
+{
+    public class JailContext : DbContext
+    {
+        public JailContext(DbContextOptions<JailContext> options) : base(options)
+        { }
+
+        public DbSet<JailAPI.Models.Jail> JailDBS { get; set; }
+    }
+}
---
+++ MilitaryRecruitmentMicroServiceV0.1/JailAPI/Models/Jail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JailAPI.Models
+{
+    public class Jail
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTimeOffset EntryDate { get; set; }
+        public DateTimeOffset ReleasDate { get; set; }
+    }
+}
