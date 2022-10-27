b50b8a5c89b04483f70dff347e273c07c61567fe
kasemsa <kasemsa2000@gmail.com>
2022-10-27 11:20:06 +0300

    remove dto

--- MilitaryRecruitmentMicroServiceV0.1/DefenseAPI/Controllers/DefenseController.cs
+++ MilitaryRecruitmentMicroServiceV0.1/DefenseAPI/Controllers/DefenseController.cs
@@ -1,5 +1,5 @@
 ﻿using DefenseAPI.Data;
-using DefenseAPI.Extensions;
+
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
